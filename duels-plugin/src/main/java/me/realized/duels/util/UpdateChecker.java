94e2ebcaaab714408bf0d71f358df650e3cb9eb1
Nahuel Dolores <contact@nahu.me>
2025-07-29 10:45:21 +0200

    Fix issues with update checker

--- duels-plugin/src/main/java/me/realized/duels/util/UpdateChecker.java
+++ duels-plugin/src/main/java/me/realized/duels/util/UpdateChecker.java
@@ -5,16 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.function.BiConsumer;
+
+import me.realized.duels.DuelsPlugin;
 import org.bukkit.plugin.Plugin;
 
 public final class UpdateChecker {
 
     private static final String API_URL = "https://api.spigotmc.org/legacy/update.php?resource=%s";
 
-    private final Plugin plugin;
+    private final DuelsPlugin plugin;
     private final int id;
 
-    public UpdateChecker(final Plugin plugin, final int id) {
+    public UpdateChecker(final DuelsPlugin plugin, final int id) {
         this.plugin = plugin;
         this.id = id;
     }
@@ -22,7 +24,7 @@ public final class UpdateChecker {
     public void check(final BiConsumer<Boolean, String> callback) {
         final String currentVersion = plugin.getDescription().getVersion();
 
-        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+        plugin.getScheduler().runTaskAsynchronously(() -> {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(String.format(API_URL, id)).openStream()))) {
                 final String latestVersion = reader.readLine();
 
